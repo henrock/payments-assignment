0de0b21a52b0d6ac9d12ea85d37f1f39241b6ff2
Henrik Jerlenius <henrik.jerlenius@gmail.com>
2022-10-17 22:21:13 +0200

    Added price_for method to Package

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,7 +2,14 @@
 
 class Package < ApplicationRecord
   has_many :prices, dependent: :destroy
+  has_many :municipality_prices, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :price_cents, presence: true
+
+  def price_for(municipality_name)    
+    municipality = Municipality.find_by!(name: municipality_name)
+    price = MunicipalityPrice.find_by!(municipality_id: municipality.id, package_id: self.id)
+    return price.price_cents
+  end
 end
