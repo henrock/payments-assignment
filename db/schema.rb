f3808ef8c2d44289f8e777311e1a8a504d0cf47e
Henrik Jerlenius <henrik.jerlenius@gmail.com>
2022-10-17 22:19:26 +0200

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_183154) do
+ActiveRecord::Schema.define(version: 2022_10_06_213104) do
+
+  create_table "municipalities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "municipality_prices", force: :cascade do |t|
+    t.integer "price_cents", default: 0, null: false
+    t.integer "municipality_id", null: false
+    t.integer "package_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["municipality_id"], name: "index_municipality_prices_on_municipality_id"
+    t.index ["package_id"], name: "index_municipality_prices_on_package_id"
+  end
 
   create_table "packages", force: :cascade do |t|
     t.integer "price_cents", default: 0, null: false
@@ -25,8 +41,12 @@ ActiveRecord::Schema.define(version: 2022_01_27_183154) do
     t.integer "package_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "municipality_id"
+    t.index ["municipality_id"], name: "index_prices_on_municipality_id"
     t.index ["package_id"], name: "index_prices_on_package_id"
   end
 
+  add_foreign_key "municipality_prices", "municipalities"
+  add_foreign_key "municipality_prices", "packages"
   add_foreign_key "prices", "packages"
 end
