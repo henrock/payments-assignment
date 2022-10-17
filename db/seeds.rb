f6d0f5aa4f790cc568fb5201a5d9b18b60b5ddbd
Henrik Jerlenius <henrik.jerlenius@gmail.com>
2022-10-17 22:19:13 +0200

    Added seeds for municipalities and prices

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,18 @@
 puts "Removing old packages and their price histories"
 Package.destroy_all
 
-puts "Creating new packages"
 
+puts "Removing old municipalities and their price histories"
+Municipality.destroy_all
+
+
+puts "Creating new municipalities"
+Municipality.insert_all(
+  YAML.load_file(Rails.root.join("import/municipalities.yaml"))
+)
+
+
+puts "Creating new packages"
 Package.insert_all(
   YAML.load_file(Rails.root.join("import/packages.yaml"))
 )
@@ -12,6 +22,19 @@ Package.insert_all(
 premium = Package.find_by!(name: "premium")
 plus = Package.find_by!(name: "plus")
 basic = Package.find_by!(name: "basic")
+
+puts "Creating municipality prices"
+municipality_prices = YAML.load_file(Rails.root.join("import/municipality_prices.yaml"))
+
+municipality_prices.each do |municipality_name, packages|
+  municipality = Municipality.find_by!(name: municipality_name)
+
+  packages.each do |package_name, price|
+    package = Package.find_by!(name: package_name)
+    price["package_id"] = package.id
+    municipality.municipality_prices.insert(price)
+  end
+end
 
 puts "Creating a price history for the packages"
 prices = YAML.load_file(Rails.root.join("import/initial_price_history.yaml"))
